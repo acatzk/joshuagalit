b88b1d5f5955dbd1ff57baed06a18e88aa5dddd3
Joshua Galit <joshuaimalay@gmail.com>
2021-02-18 21:32:35 +0800

    feat: header main footer primary design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,50 @@
+import Link from 'next/link'
+
 export default function Header () {
+  
+  const links = [
+    {
+      title: "About",
+      to: "/about"
+    }
+  ]
+
   return (
-    <h1>I am Header</h1>
+    <div className="flex items-center justify-between py-3 px-4 border-b border-gray-200">
+      <Link href="/">
+        <a className="uppercase text-xl font-light tracking-normal text-gray-700  ">
+          Joshua<span className="font-bold">Galit</span>
+        </a>
+      </Link>
+      <div>
+        <ul className="flex items-center space-x-5">
+          <li>
+            <Link href="/about">
+              <a className="text-lg text-gray-500 hover:text-gray-800 transition ease-in-out duration-150">About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a className="text-lg text-gray-500 hover:text-gray-800 transition ease-in-out duration-150">Journal</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a className="text-lg text-gray-500 hover:text-gray-800 transition ease-in-out duration-150">Project</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a className="text-lg text-gray-500 hover:text-gray-800 transition ease-in-out duration-150">Blog</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a className="text-lg text-gray-500 hover:text-gray-800 transition ease-in-out duration-150">Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   )
 }
