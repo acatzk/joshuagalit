9d50ff6fcd72e8efdaa4c009a6a5df04719abcd0
Joshua Galit <joshuaimalay@gmail.com>
2021-03-30 10:08:36 +0800

    feat: nprogress loading-bar

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import '~/styles/tailwind.css'
+import NProgress from '~/lib/nprogress'
 import { ThemeProvider } from 'next-themes'
 import 'react-perfect-scrollbar/dist/css/styles.css'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <NProgress />
       <Component {...pageProps} />
     </ThemeProvider>
   )
