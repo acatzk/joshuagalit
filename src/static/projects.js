846f7b515013b713a9af9455de33e1d6159e9e5b
acatzk <joshuaimalay@gmail.com>
2021-04-14 10:25:49 +0800

    feat: feedback form submit (#54)

---
+++ src/static/projects.js
@@ -0,0 +1,18 @@
+export const emojis = [
+  {
+    icon: '/svgs/emoji/amaze.svg',
+    text: 'amaze'
+  },
+  {
+    icon: '/svgs/emoji/happy.svg',
+    text: 'happy'
+  },
+  {
+    icon: '/svgs/emoji/unsatisfied.svg',
+    text: 'unsatisfied'
+  },
+  {
+    icon: '/svgs/emoji/cry.svg',
+    text: 'cry'
+  }
+]
