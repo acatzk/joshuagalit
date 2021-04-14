c580cfdc8e5f363c417aa9bb828ca67d7b11e245
Joshua Galit <joshuaimalay@gmail.com>
2021-04-14 18:56:53 +0800

    feat: add project viewers

--- src/graphql/mutations/index.js
+++ src/graphql/mutations/index.js
@@ -32,3 +32,27 @@ export const INSERT_FEEDBACK_MUTATION = gql`
     }
   }
 `
+
+export const INSERT_VIEWS_MUTATION = gql`
+  mutation InsertViewsMutation($project_id: uuid) {
+    insert_views(objects: {project_id: $project_id}) {
+      returning {
+        id
+        project {
+          id
+          description
+          created_at
+          slug
+          source_code_url
+          title
+          updated_at
+          views_aggregate {
+            aggregate {
+              count
+            }
+          }
+        }
+      }
+    }
+  }
+`
