1d630157a7cc9073d928da5d4fa0017df00973a0
Joshua Galit <joshuaimalay@gmail.com>
2021-04-19 11:43:12 +0800

    chore: update exports svg icons

---
+++ src/components/ProjectCommentForm.js
@@ -0,0 +1,113 @@
+import { useForm } from 'react-hook-form'
+import { GitHubIcon, ExternalLinkIcon, MessageIcon } from '~/utils/Icons' 
+
+export default function ProjectCommentForm ({ ...projects }) {
+  
+  const handleComment = ({ name, comment }) => {
+    alert(JSON.stringify({ name, comment }))
+  }
+
+  return (
+    <div className="flex">
+      <div className="w-full">
+        <div className="flex items-center justify-between">
+          <Tabs {...projects[0]} />
+        </div>
+        <div className="border-t border-gray-200 dark:border-gray-700 space-y-4">
+          <div className="flex space-x-3 py-4 px-2">
+            <Avatar />
+            <div className="rounded-b-xl rounded-r-xl px-4 py-4 bg-gray-100 dark:bg-gray-800 w-full transition ease-in-out duration-700">
+              <CommentForm 
+                onSubmit={handleComment}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function CommentForm ({ onSubmit }) {
+
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { isSubmitting, isDirty, isValid }
+  } = useForm({ mode: 'onChange' })
+
+  return (
+    <form className="space-y-2" onSubmit={handleSubmit(onSubmit)}>
+      <div className="space-y-6">
+        <div>
+          <input 
+            type="text" 
+            placeholder="Name" 
+            name="name"
+            ref={register({ required: true })}
+            disabled={isSubmitting}
+            className="border-b-2 border-gray-300 dark:border-gray-700 focus:border-blue-twitter dark:focus:border-blue-twitter bg-transparent border-0 w-full py-1 focus:outline-none focus:ring-0 transition ease-in-out duration-200 disabled:cursor-not-allowed disabled:opacity-50" 
+          />
+        </div>
+        <div>
+          <textarea 
+            placeholder="Add a public comment..." 
+            name="comment"
+            ref={register({ required: true })}
+            disabled={isSubmitting}
+            className="border-b-2 border-gray-300 dark:border-gray-700 focus:border-blue-twitter dark:focus:border-blue-twitter bg-transparent border-0 w-full py-1 focus:outline-none focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50" 
+          />
+        </div>
+      </div>
+      <div className="float-right">
+        <button
+          type="submit"
+          disabled={!isDirty || !isValid}
+          className="px-4 py-2 font-medium text-sm bg-blue-twitter text-white focus:outline-none rounded disabled:cursor-not-allowed disabled:bg-gray-300 dark:disabled:bg-gray-400 disabled:text-gray-800"
+        >
+          Comment
+        </button>
+      </div>
+    </form>
+  )
+}
+
+function Tabs ({ source_code_url, demo_url }) {
+  return (
+    <div>
+      <ul className="flex items-center text-sm">
+        <li className="border-b-2 border-transparent border-blue-twitter px-3">
+          <button className="flex items-center  space-x-2 focus:outline-none pb-2 font-semibold text-blue-twitter">
+            <MessageIcon className="w-4 h-4" />
+            <span>Comments</span>
+          </button>
+        </li>
+        {source_code_url && (
+          <li className="border-b-2 border-transparent hover:border-gray-300 dark:hover:border-white transition ease-in duration-150 px-3">
+            <a href={source_code_url} target="_blank" className="flex items-center space-x-2 pb-2 text-gray-600 dark:text-gray-400 dark:hover:text-white">
+              <GitHubIcon className="w-4 h-4 fill-current" />
+              <span className="text-sm line-clamp-1">Source Code</span>
+            </a>
+          </li>
+        )}
+        {demo_url && (
+          <li className="border-b-2 border-transparent hover:border-gray-300 dark:hover:border-white transition ease-in duration-100 px-3">
+            <a href={demo_url} target="_blank" className="flex items-center space-x-2 pb-2 text-gray-600 dark:text-gray-400 dark:hover:text-white">
+              <ExternalLinkIcon className="w-4 h-4" />
+              <span className="text-sm line-clamp-1">Demo</span>
+            </a>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+function Avatar () {
+  return (
+    <div className="flex-shrink-0">
+      <img className="w-10 h-10 rounded-full" src="/images/default-avatar.jpg" />
+    </div>
+  )
+}
+
--- src/components/ProjectPost.js
+++ src/components/ProjectPost.js
@@ -1,6 +1,7 @@
 import Moment from 'react-moment'
-import ProjectTab from './ProjectTab'
 import SponsorCard from '~/components/SponsorCard'
+import { ViewsIcon, LikeIcon } from '~/utils/Icons'
+import ProjectCommentForm from './ProjectCommentForm'
 
 export default function ProjectPost ({ projects }) {
   const { 
@@ -27,7 +28,7 @@ export default function ProjectPost ({ projects }) {
         <div className="flex flex-wrap items-center space-x-1.5 text-gray-500">
           <div className="flex flex-wrap items-center space-x-1">
             <span className="text-xs font-medium mt-0.5">{ count }</span>
-            <ViewsIcon />
+            <ViewsIcon className="w-4 h-4" />
           </div>
           <span>&bull;</span>
           <button 
@@ -37,7 +38,7 @@ export default function ProjectPost ({ projects }) {
             hover:text-black dark:hover:text-white px-1 py-0.5 focus:outline-none 
             transition ease-in-out duration-200">
             <span>
-              <LikeIcon />
+              <LikeIcon className="w-3.5 h-3.5" />
             </span>
             <span className="text-xs">45</span>
           </button>
@@ -49,25 +50,8 @@ export default function ProjectPost ({ projects }) {
           <img src={project_image_url} />
         </div>
       </div>
-      <ProjectTab {...projects} />
+      <ProjectCommentForm {...projects} />
       <SponsorCard />
     </div>
-  )
-}
-
-function ViewsIcon () {
-  return (
-    <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-    </svg>
-  )
-}
-
-function LikeIcon () {
-  return (
-    <svg className="w-3.5 h-3.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path>
-    </svg>
   )
 }
