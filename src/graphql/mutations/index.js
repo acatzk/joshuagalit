8021e45265320147bdd0ee349870089dc6b7d428
Joshua Galit <joshuaimalay@gmail.com>
2021-04-24 09:07:48 +0800

    refactor: contact email with emailjs provider

--- src/graphql/mutations/index.js
+++ src/graphql/mutations/index.js
@@ -1,16 +1,5 @@
 import { gql } from 'graphql-request'
 
-export const INSERT_MAIL_MUTATION = gql`
-  mutation InsertMailMutation($name: String!, $email: String!, $message: String!) {
-    insert_email_employer(objects: {name: $name, email: $email, message: $message}) {
-      affected_rows
-      returning {
-        id
-      }
-    }
-  }
-`
-
 export const DELETE_MAIL_MUTATION = gql`
   mutation DeleteEmailById($id: uuid!) {
     delete_email_employer(where: {id: {_eq: $id}}) {
