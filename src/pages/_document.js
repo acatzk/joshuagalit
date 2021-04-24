184366681ff0f9fb168b46a6c8d35156b2aa3ea7
Joshua Galit <joshuaimalay@gmail.com>
2021-04-24 12:20:53 +0800

    fix: Nunito google font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
             rel="stylesheet" />
           <link 
-            href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,800;1,900&display=swap" 
+            href="https://fonts.googleapis.com/css2?family=Nunito:wght@600;700;800;900&display=swap" 
             rel="stylesheet" />
         </Head>
         <body>
