672ce031a99f9436a731b887da02cdada31919ca
acatzk <joshuaimalay@gmail.com>
2021-05-06 14:33:23 +0800

    feat: blog show count (#100)

--- src/components/BlogHeader.js
+++ src/components/BlogHeader.js
@@ -1,9 +1,9 @@
 import { SearchIcon } from '~/utils/Icons'
 
-export default function BlogHeader () {
+export default function BlogHeader ({ count }) {
   return (
     <div className="mt-4 md:mt-10 space-y-5 pb-4 border-b border-gray-200 dark:border-gray-700">
-      <h1 className="text-3xl md:text-5xl font-black">Blog List</h1>
+      <h1 className="text-3xl md:text-5xl font-black">Blog ({ count })</h1>
       <div className="relative flex items-center max-w-lg">
         <div className="absolute pl-4 pt-2.5">
           <SearchIcon className="h-7 w-7 fill-current text-gray-400" />
