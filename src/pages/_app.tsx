3b8ef8a8fca9986ebe6ba530aa437a37efee71a1
acatzk <joshuaimalay@gmail.com>
2021-06-20 08:58:50 +0800

    feat: use nprogress by @tanem/react-nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,16 @@
-import Head from 'next/head'
+import '~/styles/global.css'
 import '~/styles/tailwind.css'
-import NProgress from '~/lib/nprogress'
 import { ThemeProvider } from 'next-themes'
-import 'react-mde/lib/styles/css/react-mde-all.css'
-import 'react-perfect-scrollbar/dist/css/styles.css'
+import NProgress from '~/lib/react-nprogress'
 import { ToastProvider } from 'react-toast-notifications'
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-      </Head>
-      <ThemeProvider attribute="class">
-        <NProgress />
-        <ToastProvider>
-          <Component {...pageProps} />
-        </ToastProvider>
-      </ThemeProvider>
-    </>
+    <ThemeProvider attribute="class">
+      <NProgress />
+      <ToastProvider>
+        <Component {...pageProps} />
+      </ToastProvider>
+    </ThemeProvider>
   )
 }
