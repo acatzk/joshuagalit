0410d9fd91c1fe6ea25bc46f202122c3bd2e9508
acatzk <joshuaimalay@gmail.com>
2021-07-09 16:13:11 +0800

    feat: initial modal sign in buttons

---
+++ src/components/LoginModal.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { Transition } from '@headlessui/react';
+import { fadeInUp, stagger } from '~/animation';
+import { FaGithub, FaGoogle, FaFacebook } from 'react-icons/fa';
+
+interface LoginWIthModalProps {
+  isOpen: boolean;
+  setIsOpen: Function;
+}
+
+const LoginWithModal: React.FC<LoginWIthModalProps> = ({
+  isOpen,
+  setIsOpen,
+}) => {
+  const loginWith = [
+    {
+      title: 'Facebook',
+      Icon: FaFacebook,
+      color: '#4267B2',
+    },
+    {
+      title: 'Google',
+      Icon: FaGoogle,
+      color: '#db3236',
+    },
+    {
+      title: 'GitHub',
+      Icon: FaGithub,
+      color: '#24292e',
+    },
+  ];
+
+  return isOpen ? (
+    <Transition show={isOpen} as="div" static className="fixed z-10 inset-0">
+      <div className="flex items-center justify-center min-h-screen text-center sm:block sm:p-0">
+        <div
+          onClick={() => setIsOpen(false)}
+          className="fixed inset-0 bg-gray-800 bg-opacity-75 transition-opacity"
+        />
+
+        {/* This element is to trick the browser into centering the modal contents. */}
+        <motion.div variants={fadeInUp}>
+          <span
+            className=" sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <div className="inline-block align-top rounded-lg overflow-hidden shadow-2xl transform transition-all sm:align-middle sm:max-w-lg sm:w-full">
+            <div className="bg-white dark:bg-dark-dim px-4 py-6 transition ease-in-out duration-700">
+              <div className="flex flex-col items-center space-y-3">
+                <h1 className="text-2xl font-extrabold text-black dark:text-white">
+                  Login with
+                </h1>
+                <motion.div
+                  variants={stagger}
+                  className="flex flex-col space-y-2 w-full"
+                >
+                  {loginWith.map(({ title, Icon, color }, i) => (
+                    <motion.button
+                      key={i}
+                      variants={fadeInUp}
+                      className={`group flex items-center justify-center space-x-2 py-3 px-4 rounded 
+                        bg-[${color}] hover:bg-opacity-80 transition ease-in-out duration-200
+                        focus:outline-none border border-transparent
+                        active:bg-transparent active:border-black dark:active:border-white`}
+                    >
+                      <Icon className="w-5 h-5 text-white group-active:text-black dark:group-active:text-white" />
+                      <span className="font-semibold text-white group-active:text-black dark:group-active:text-white">
+                        Continue with {title}
+                      </span>
+                    </motion.button>
+                  ))}
+                </motion.div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      </div>
+    </Transition>
+  ) : null;
+};
+
+export default LoginWithModal;
--- src/components/MyDayHeader.tsx
+++ src/components/MyDayHeader.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import LoginWithModal from './LoginModal';
+import React, { useState, useRef } from 'react';
 
 const MyDayHeader: React.FC<{}> = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="w-full py-3 px-4 mt-2 md:mt-8 border-b border-gray-200 dark:border-gray-700">
       <div className="flex items-center justify-between">
-        <h1 className="font-extrabold text-2xl">MyDay Posts</h1>
-        <button
-          type="button"
-          className="
-            text-base px-4 flex justify-center py-1 rounded-md border border-transparent bg-black 
-          dark:bg-white text-white focus:outline-none font-medium disabled:opacity-50
-           disabled:cursor-not-allowed dark:text-black hover:bg-white dark:hover:bg-transparent border-black
-           hover:text-black dark:hover:text-white hover:border-black dark:hover:border-gray-400 dark:border-transparent
-            transition ease-in-out duration-200 active:scale-150"
-        >
-          Sign In
-        </button>
+        <h1 className="font-extrabold text-2xl">MyDay</h1>
+        <div>
+          <button
+            type="button"
+            onClick={() => setIsOpen(!isOpen)}
+            className="
+              text-base font-medium px-4 py-1 rounded-md focus:outline-none
+              border border-black bg-transparent 
+            text-black hover:text-white hover:bg-black hover:border-black
+            dark:text-white dark:bg-transparent dark:hover:bg-white dark:border-white dark:hover:text-black
+              transition ease-in-out duration-200 disabled:opacity-50 disabled:cursor-not-allowed"
+          >
+            Sign In
+          </button>
+          <LoginWithModal isOpen={isOpen} setIsOpen={setIsOpen} />
+        </div>
       </div>
     </header>
   );
