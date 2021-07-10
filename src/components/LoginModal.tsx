2ffe2c737593b13714b7b4542e0c7e39e5704cf2
acatzk <joshuaimalay@gmail.com>
2021-07-10 08:43:08 +0800

    fix(color): update modal login button colors

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { Transition } from '@headlessui/react';
 import { fadeInUp, stagger } from '~/animation';
-import { FaGithub, FaGoogle, FaFacebook } from 'react-icons/fa';
+import { FaGithub, FaGoogle, FaFacebook, FaGit } from 'react-icons/fa';
 
 interface LoginWIthModalProps {
   isOpen: boolean;
@@ -13,24 +13,6 @@ const LoginWithModal: React.FC<LoginWIthModalProps> = ({
   isOpen,
   setIsOpen,
 }) => {
-  const loginWith = [
-    {
-      title: 'Facebook',
-      Icon: FaFacebook,
-      color: '#4267B2',
-    },
-    {
-      title: 'Google',
-      Icon: FaGoogle,
-      color: '#db3236',
-    },
-    {
-      title: 'GitHub',
-      Icon: FaGithub,
-      color: '#24292e',
-    },
-  ];
-
   return isOpen ? (
     <Transition show={isOpen} as="div" static className="fixed z-10 inset-0">
       <div className="flex items-center justify-center min-h-screen text-center sm:block sm:p-0">
@@ -47,7 +29,7 @@ const LoginWithModal: React.FC<LoginWIthModalProps> = ({
           >
             &#8203;
           </span>
-          <div className="inline-block align-top rounded-lg overflow-hidden shadow-2xl transform transition-all sm:align-middle sm:max-w-lg sm:w-full">
+          <div className="inline-block align-top rounded-lg overflow-hidden shadow-2xl transform sm:align-middle sm:max-w-lg sm:w-full">
             <div className="bg-white dark:bg-dark-dim px-4 py-6 transition ease-in-out duration-700">
               <div className="flex flex-col items-center space-y-3">
                 <h1 className="text-2xl font-extrabold text-black dark:text-white">
@@ -57,21 +39,42 @@ const LoginWithModal: React.FC<LoginWIthModalProps> = ({
                   variants={stagger}
                   className="flex flex-col space-y-2 w-full"
                 >
-                  {loginWith.map(({ title, Icon, color }, i) => (
-                    <motion.button
-                      key={i}
-                      variants={fadeInUp}
-                      className={`group flex items-center justify-center space-x-2 py-3 px-4 rounded 
-                        bg-[${color}] hover:bg-opacity-80 transition ease-in-out duration-200
+                  <motion.button
+                    variants={fadeInUp}
+                    className={`group flex items-center justify-center space-x-2 py-3 px-4 rounded 
+                        bg-[#4267B2] hover:bg-opacity-80 transition ease-in-out duration-200
                         focus:outline-none border border-transparent
                         active:bg-transparent active:border-black dark:active:border-white`}
-                    >
-                      <Icon className="w-5 h-5 text-white group-active:text-black dark:group-active:text-white" />
-                      <span className="font-semibold text-white group-active:text-black dark:group-active:text-white">
-                        Continue with {title}
-                      </span>
-                    </motion.button>
-                  ))}
+                  >
+                    <FaFacebook className="w-5 h-5 text-white group-active:text-black dark:group-active:text-white" />
+                    <span className="font-semibold text-white group-active:text-black dark:group-active:text-white">
+                      Continue with Facebook
+                    </span>
+                  </motion.button>
+                  <motion.button
+                    variants={fadeInUp}
+                    className={`group flex items-center justify-center space-x-2 py-3 px-4 rounded 
+                        bg-[#db3236] hover:bg-opacity-80 transition ease-in-out duration-200
+                        focus:outline-none border border-transparent
+                        active:bg-transparent active:border-black dark:active:border-white`}
+                  >
+                    <FaGoogle className="w-5 h-5 text-white group-active:text-black dark:group-active:text-white" />
+                    <span className="font-semibold text-white group-active:text-black dark:group-active:text-white">
+                      Continue with Google
+                    </span>
+                  </motion.button>
+                  <motion.button
+                    variants={fadeInUp}
+                    className={`group flex items-center justify-center space-x-2 py-3 px-4 rounded 
+                        bg-[#24292e] hover:bg-opacity-80 transition ease-in-out duration-200
+                        focus:outline-none border border-transparent
+                        active:bg-transparent active:border-black dark:active:border-white`}
+                  >
+                    <FaGithub className="w-5 h-5 text-white group-active:text-black dark:group-active:text-white" />
+                    <span className="font-semibold text-white group-active:text-black dark:group-active:text-white">
+                      Continue with GitHub
+                    </span>
+                  </motion.button>
                 </motion.div>
               </div>
             </div>
