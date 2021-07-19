f845708ed3768401119caa09e387a30a90c5919a
acatzk <joshuaimalay@gmail.com>
2021-07-19 14:16:24 +0800

    ✨ FEATURE:  Add nextjs-nextprogress dep

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,14 @@ import '~/styles/global.css'
 import '~/styles/tailwind.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
-import NProgress from '~/lib/react-nprogress'
+import NextProgress from '~/lib/next-progress'
 import { AnimatePresence } from 'framer-motion'
 import { ToastProvider } from 'react-toast-notifications'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider attribute="class">
-      <NProgress />
+      <NextProgress />
       <ToastProvider>
         <AnimatePresence exitBeforeEnter>
           <Component {...pageProps} />
