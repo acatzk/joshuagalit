9a27349c1f940723b38a308a0107ec374bbd1ee2
acatzk <joshuaimalay@gmail.com>
2022-06-10 08:30:29 +0800

    refactor: add src folder & absolute imports

---
+++ src/components/projects/ProjectFeedbackForm.tsx
@@ -0,0 +1,98 @@
+import Image from 'next/image'
+import { emojis } from '~/mock/data'
+import Loading from '~/utils/Loading'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+interface FeedbackProps {
+  onSubmit: any
+}
+
+const FeedbackForm: React.FC<FeedbackProps> = ({ onSubmit }) => {
+  const [emoji, setEmoji] = useState('amaze')
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors }
+  } = useForm()
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-3">
+      <div className="px-4">
+        <input
+          type="text"
+          autoFocus
+          disabled={isSubmitting}
+          placeholder="Name"
+          {...register('name', {
+            required: true
+          })}
+          className="w-full text-sm rounded-lg border border-gray-400 focus:ring-0 focus:border-gray-900 bg-white dark:bg-dark-dim dark:focus:border-gray-300 disabled:opacity-50 disabled:cursor-not-allowed transition ease-in-out duration-150"
+        />
+        <div className="space-y-0.5 ml-1.5">
+          {errors.name?.type === 'required' && (
+            <span className="text-xs text-red-500 font-medium">Name is required</span>
+          )}
+        </div>
+      </div>
+      <div className="px-4 flex flex-col space-y-1">
+        <textarea
+          rows={4}
+          cols={50}
+          disabled={isSubmitting}
+          placeholder="Your feedback..."
+          {...register('message', {
+            required: true
+          })}
+          className="w-full text-sm rounded-lg border border-gray-400 focus:ring-0 focus:border-gray-900 bg-white dark:bg-dark-dim dark:focus:border-gray-300 disabled:opacity-50 disabled:cursor-not-allowed transition ease-in-out duration-150"
+        ></textarea>
+        {errors.message && (
+          <span className="pl-1 text-xs text-red-500 font-medium pt-0.5">
+            {errors.message.message}
+          </span>
+        )}
+        <div className="space-y-0.5 ml-1.5">
+          {errors.message?.type === 'required' && (
+            <span className="text-xs text-red-500 font-medium">Message is required</span>
+          )}
+        </div>
+      </div>
+      <div className="flex items-center justify-between px-3 border-t border-gray-200 dark:border-gray-700 py-1.5 bg-gray-50 dark:bg-gray-800">
+        {/* <div className="flex items-center space-x-3">
+          {emojis.map(({ icon, text }, i) => (
+            <div
+              key={i}
+              onClick={() => setEmoji(text)}
+              className={`${
+                emoji === text && 'ring-2 ring-offset-2 ring-yellow-300 bg-transparent'
+              } relative h-[25px] w-[25px] cursor-pointer rounded-full bg-transparent p-1 dark:bg-gray-700 
+              focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed transition ease-in-out duration-150 transform hover:scale-110`}
+            >
+              <input
+                type="text"
+                value={emoji}
+                onChange={(e) => setEmoji(e.target.value)}
+                className="hidden w-5 h-5"
+                {...register('emoji')}
+              />
+              <Image layout="fill" src={icon} alt="Icon" />
+            </div>
+          ))}
+        </div> */}
+        <button
+          type="submit"
+          disabled={isSubmitting}
+          className={`text-sm px-2 w-14 flex justify-center py-1 rounded-md border border-transparent bg-black dark:bg-white text-white transition ease-in-out duration-200 focus:outline-none font-medium disabled:opacity-50 disabled:cursor-not-allowed ${
+            !isSubmitting
+              ? 'dark:text-black hover:bg-white dark:hover:bg-black hover:text-black dark:hover:text-white hover:border-black dark:hover:border-gray-400'
+              : 'dark:text-white hover:bg-black dark:hover:bg-white hover:text-white dark:hover:text-black hover:border-white'
+          }`}
+        >
+          {isSubmitting ? <Loading className="w-5 h-5 text-white dark:text-black" /> : 'Send'}
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default FeedbackForm
---
+++ src/components/projects/ProjectPost.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import Image from 'next/image'
+import Moment from 'react-moment'
+import { FiEye } from 'react-icons/fi'
+import ProjectComment from './ProjectComment'
+import { BiMessageRounded } from 'react-icons/bi'
+import SponsorCard from '~/components/SponsorCard'
+
+interface ProjectPostProps {
+  projects: any
+  mutate?: any
+}
+
+const ProjectPost: React.FC<ProjectPostProps> = ({ projects, mutate }) => {
+  const {
+    id,
+    title,
+    description,
+    created_at,
+    project_image_url,
+    views_aggregate: {
+      aggregate: { viewsCount }
+    },
+    comments_aggregate: {
+      aggregate: { commentsCount }
+    }
+  } = projects[0]
+
+  return (
+    <div key={id} className="px-4 space-y-6 pb-10">
+      <div className="flex items-center justify-center">
+        <h1 className="font-semibold text-2xl md:text-3xl tracking-wide">{title}</h1>
+      </div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-2">
+          <div className="flex-shrink-0">
+            <Image
+              width={28}
+              height={28}
+              className="border-gray-200 dark:border-gray-700 rounded-full bg-gray-200 dark:bg-gray-800"
+              src="/images/my-avatar.jpg"
+              alt="My Profile Image"
+              layout="intrinsic"
+            />
+          </div>
+          <h1 className="text-sm tracking-tight text-gray-700 dark:text-gray-400 line-clamp-1">
+            Joshua Galit / <Moment date={created_at} format="MMM DD, YYYY" />
+          </h1>
+        </div>
+        <div className="flex items-center space-x-1 text-gray-500">
+          <div className="flex items-center space-x-1 cursor-default">
+            <span className="text-xs font-medium mt-0.5 line-clamp-1">{commentsCount}</span>
+            <BiMessageRounded className="w-4 h-4" />
+          </div>
+          <span>&middot;</span>
+          <div className="flex items-center space-x-1 cursor-default">
+            <span className="text-xs font-medium mt-0.5 line-clamp-1">{viewsCount}</span>
+            <FiEye className="w-4 h-4" />
+          </div>
+        </div>
+      </div>
+      <div className="space-y-1">
+        <h1 className="text-base text-gray-700 dark:text-gray-400">{description}</h1>
+        <div className="flex-shrink-0 overflow-hidden rounded-lg ring-1 ring-gray-300 dark:ring-gray-600 relative">
+          <Image
+            src={project_image_url}
+            width={1000}
+            height={550}
+            alt={title}
+            blurDataURL={project_image_url}
+            placeholder="blur"
+            layout="responsive"
+            className="bg-gray-200 dark:bg-gray-800 transition ease-in-out duration-700"
+          />
+        </div>
+      </div>
+      <ProjectComment mutate={mutate} {...projects} />
+      <SponsorCard />
+    </div>
+  )
+}
+
+export default ProjectPost
