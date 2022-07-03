e8cefb45a3a2f14934a6a60dd37c1b6ba962376b
acatzk <joshuaimalay@gmail.com>
2022-07-03 09:46:54 +0800

    feat: add my github details

--- src/layouts/aboutPageLayout.tsx
+++ src/layouts/aboutPageLayout.tsx
@@ -1,19 +1,41 @@
+import useSWR from 'swr'
 import React from 'react'
+import { GetStaticProps } from 'next'
 import Navbar from '~/components/index/Navbar'
 import Sidebar from '~/components/index/Sidebar'
 
 type Props = {
   children: React.ReactNode
+  prefetchedData?: any
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch('https://api.github.com/users/acatzk')
+  const prefetchedData = await res.json()
+
+  return {
+    props: {
+      prefetchedData
+    }
+  }
 }
 
 const AboutPageLayout: React.FC<Props> = (props) => {
-  const { children } = props
+  const { children, prefetchedData } = props
+
+  const { data } = useSWR(
+    'https://api.github.com/users/acatzk',
+    async (query) => await fetch(query).then((res) => res.json()),
+    {
+      fallbackData: prefetchedData
+    }
+  )
 
   return (
     <div className="flex items-center justify-center h-screen min-h-screen px-4 py-4 md:py-0">
       <div className="grid grid-cols-12 gap-6 w-full max-w-6xl">
         <div className="p-4 text-center col-span-12 lg:col-span-4 xl:col-span-3 rounded-2xl">
-          <Sidebar />
+          <Sidebar githubDetails={data} />
         </div>
         <div className="flex flex-col col-span-12 lg:col-span-8 xl:col-span-9 rounded-2xl">
           <Navbar />
